1651a2c9037091ea3eecf959aa4b4d558d133814
gloryumoh <gumoh@claflin.edu>
2024-11-20 17:51:34 +0200

    Add files via upload

---
+++ Chi-square test analysis.R
@@ -0,0 +1,12 @@
+ # Author: Glory, Date: 20/11/2024, Purpose: Calculate Chi-square analysis.
+
+ #Read dummy dataset
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+ 
+  #Apply the Chi square function
+ chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+ 
+     Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
